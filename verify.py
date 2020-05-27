6372a0d8c0b6bdc1a3a8d9b31b925abb0f1c8945
TSP66 <62830753+TSP66@users.noreply.github.com>
2020-05-28 09:33:21 +1000

    Python file transfer

--- verify.py
+++ verify.py
@@ -5,10 +5,17 @@ from miner_good import *
 
 FILE = open("US_TEXT.txt", "r", encoding="utf-8")
 data = FILE.readline()
+print("DAta:", data)
 FILE.close()
 
-transfer_text = open("transfer_PythonC.txt", "w")
+file2 = open("transfer_PythonC.txt", "w")
+file2.write(str(md2(data)))
+blank = md2("helloworld") #Leave this line alone, it gives time to the above lines to process
 
+file2.close()
+
+transfer_text = open("transfer_PythonC.txt", "a")
 transfer_text.write(str(md2(data)))
+
 blank = md2("helloworld") #Leave this line alone, it gives time to the above lines to process
 transfer_text.close()
