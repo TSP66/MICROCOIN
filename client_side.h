bfaf3ca58bf7e236117cffbe31b00b2e56c9824c
TSP66 <62830753+TSP66@users.noreply.github.com>
2020-06-12 18:36:28 +1000

    Put stuff in seperate function

--- client_side.h
+++ client_side.h
@@ -1,20 +1,23 @@
+//client_side.h
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "sockets.h"
+#include <string.h>
 
 struct connection conts;
 
-char first_part[10] = "10.1.1.";
+const char*generateip(int i){
+   //printf("%d", i);
+   // char ** var;
+   char first_part[11] = "10.1.1.";
 
-
-void scanports(){
- for(int i = 1; i < 255; i++){
    char first = (char) floor(i/100)+ 48;
-  
+   //printf("%c", first);
    char second = (char) floor((i-((first-48)*100))/10) + 48;
-
+   //printf("%c", second);
    char third = (char) i - (first-48)*100 - (second-48)*10 + 48;
-
+   //printf("%c", third);
    int pos = 7;
    char * zero = "0";
    if(first != *zero) {
@@ -26,11 +29,32 @@ void scanports(){
       pos++;
    }
    
-      first_part[pos] = third;
-      pos++;
+   first_part[pos] = third;
+
+
+  
+   char  *dita = first_part;
+    char * var = (char *) malloc(11);;
+    sprintf(var, "%s", dita);
+    
+  //  return (const char *) d
+    
+    return var;
+   //d =  ip;
+}
+
+void scanports(){
+ for(int i = 1; i < 255; i++){
+  
    
-  char * ip = first_part;
-  puts(ip);
+   //  char * ipa = (char *) generateip(i);
+  
+  //conts.ip = ipa;
+   //  char *d;
+     char *d;
+    d = (char *) generateip(i);
+     puts(d);
 
  }
 }
+
