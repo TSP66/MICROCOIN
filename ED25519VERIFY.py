9e5f8891a38dc1d806062d60cb32bb14680c4936
TSP66 <62830753+TSP66@users.noreply.github.com>
2020-08-21 18:42:50 +1000

    Update ED25519VERIFY.py

--- ED25519VERIFY.py
+++ ED25519VERIFY.py
@@ -6,16 +6,19 @@ import sys
 
 info = (sys.argv[1])
        
-alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","6","7","8","9"]
+#alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","6","7","8","9"]
 
-privKey, pubKey = ed25519.create_keypair()
-print("Private key (32 bytes):", privKey.to_ascii(encoding='hex'))
-print("Public key (32 bytes): ", pubKey.to_ascii(encoding='hex'))
 
-msg = b'_SIGNATURE_'
-msg = msg + str(info).encode("utf-8")
+f1 = open("PRIVATE_KEY.txt", "r")
+privKey = f1.read()
+f1.close()
+f2 = open("PUBLIC_KEY.txt", "r")
+pubKey = f2.read()
+f2.close()
 
-print(msg)
+
+
+msg = bytearray(info, "utf-8")
 
 signature = privKey.sign(msg, encoding='hex')
 print("Signature (64 bytes):", signature)
